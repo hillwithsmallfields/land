7a1b359234931c846c151a0d97d1af75de31d07d
John Sturdy <jcg.sturdy@gmail.com>
2021-10-16 18:44:43 +0100

    Trying to get emacs modules loaded despite the package manager not doing anything useful

--- land.el
+++ land.el
@@ -1,7 +1,13 @@
 (load-library "package")
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages"))
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+;; this one seems to have become inactive:
+;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(package-initialize t)
 (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 (package-refresh-contents)
-(dolist (p '("dash" "dash-functional" "ht" "org-ql" "org-super-agenda" "ov" "peg" "s" "ts"))
+(dolist (p '("dash"
+	     "dash-functional"
+	     "ht" "org-ql" "org-super-agenda" "ov" "peg" "s" "ts"))
+  (message "installing %s" p)
   (package-install p))
