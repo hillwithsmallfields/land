f7c19bb44672ec685f24b7fc11d985022453eefe
John Sturdy <jcg.sturdy@gmail.com>
2021-10-16 15:03:18 +0100

    Add emacs startup --- probably to be integrated into main script later

---
+++ land.el
@@ -0,0 +1,7 @@
+(load-library "package")
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages"))
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(package-refresh-contents)
+(dolist (p '("dash" "dash-functional" "ht" "org-ql" "org-super-agenda" "ov" "peg" "s" "ts"))
+  (package-install p))
